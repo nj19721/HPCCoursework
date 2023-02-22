a6860bbff9979b63fcfb4d4327e9562bfed24a02
Alice <nj19721@bristol.ac.uk>
2023-02-22 15:06:41 +0000

    math-optimisation-maybe

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 EXE=d2q9-bgk
 
 CC=gcc
-CFLAGS= -std=c99 -Wall -O3
+CFLAGS= -std=c99 -Wall -Ofast -mtune=native
 LIBS = -lm
 
 FINAL_STATE_FILE=./final_state.dat
--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -282,6 +282,9 @@ int reboundCollisionAVVels(const t_param params, t_speed* cells, t_speed* tmp_ce
   {
     for (int ii = 0; ii < params.nx; ii++)
     {
+      /* determine indices of axis-direction neighbours
+      ** respecting periodic boundary conditions (wrap around) */
+
       /* don't consider occupied cells */
       if (obstacles[jj*params.nx + ii])
       {
@@ -342,36 +345,19 @@ int reboundCollisionAVVels(const t_param params, t_speed* cells, t_speed* tmp_ce
         /* zero velocity density: weight w0 */
         d_equ[0] = w0 * local_density
                    * (1.f - u_sq / (2.f * c_sq));
-        /* axis speeds: weight w1 */
-        d_equ[1] = w1 * local_density * (1.f + u[1] / c_sq
-                                         + (u[1] * u[1]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        d_equ[2] = w1 * local_density * (1.f + u[2] / c_sq
-                                         + (u[2] * u[2]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        d_equ[3] = w1 * local_density * (1.f + u[3] / c_sq
-                                         + (u[3] * u[3]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        d_equ[4] = w1 * local_density * (1.f + u[4] / c_sq
-                                         + (u[4] * u[4]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        /* diagonal speeds: weight w2 */
-        d_equ[5] = w2 * local_density * (1.f + u[5] / c_sq
-                                         + (u[5] * u[5]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        d_equ[6] = w2 * local_density * (1.f + u[6] / c_sq
-                                         + (u[6] * u[6]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        d_equ[7] = w2 * local_density * (1.f + u[7] / c_sq
-                                         + (u[7] * u[7]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
-        d_equ[8] = w2 * local_density * (1.f + u[8] / c_sq
-                                         + (u[8] * u[8]) / (2.f * c_sq * c_sq)
-                                         - u_sq / (2.f * c_sq));
+        cells[ii + jj*params.nx].speeds[0] = tmp_cells[ii + jj*params.nx].speeds[0]
+                                                  + params.omega
+                                                  * (d_equ[0] - tmp_cells[ii + jj*params.nx].speeds[0]);
+        /* set weight to w0 for first iteration of loop */
+        float w = w1;
 
         /* relaxation step */
-        for (int kk = 0; kk < NSPEEDS; kk++)
+        for (int kk = 1; kk < NSPEEDS; kk++)
         {
+          if(kk > 4) w = w2;
+          d_equ[kk] = w * local_density * (1.f + u[kk] / c_sq
+                                         + (u[kk] * u[kk]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
           cells[ii + jj*params.nx].speeds[kk] = tmp_cells[ii + jj*params.nx].speeds[kk]
                                                   + params.omega
                                                   * (d_equ[kk] - tmp_cells[ii + jj*params.nx].speeds[kk]);
