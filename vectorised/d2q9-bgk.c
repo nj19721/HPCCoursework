fd907a08079ec785a5c558c213fce8d8c54a8b1a
Alice <nj19721@bristol.ac.uk>
2023-03-03 15:09:03 +0000

    im dying out here scoob

--- vectorised/d2q9-bgk.c
+++ vectorised/d2q9-bgk.c
@@ -293,7 +293,7 @@ int reboundCollisionAVVels(const t_param params, t_speed* restrict cells, t_spee
   #pragma omp simd aligned(cells, tmp_cells) reduction(+:tot_cells, tot_u)
   for (int jj = 0; jj < params.ny; jj++)
   {
-    #pragma omp simd aligned(cells, tmp_cells) reduction(+:tot_cells, tot_u)
+    #pragma omp simd aligned(cells, tmp_cells)
     for (int ii = 0; ii < params.nx; ii++)
     {
       /* determine indices of axis-direction neighbours
