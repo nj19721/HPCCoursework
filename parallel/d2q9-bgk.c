a168816afc3ffe6bde7d2b146da10e7ffa3dbe24
Alice <nj19721@bristol.ac.uk>
2023-03-04 14:43:10 +0000

    test

--- parallel/d2q9-bgk.c
+++ parallel/d2q9-bgk.c
@@ -295,10 +295,10 @@ int reboundCollisionAVVels(const t_param params, t_speed* cells, t_speed* tmp_ce
       ** respecting periodic boundary conditions (wrap around) */
 
       /* don't consider occupied cells */
-      if (obstacles[jj*params.nx + ii])
+      /*if (obstacles[jj*params.nx + ii])
       {
-        /* called after propagate, so taking values from scratch space
-        ** mirroring, and writing into main grid */
+        // called after propagate, so taking values from scratch space
+        ** mirroring, and writing into main grid
         cells[ii + jj*params.nx].speeds[1] = tmp_cells[ii + jj*params.nx].speeds[3];
         cells[ii + jj*params.nx].speeds[2] = tmp_cells[ii + jj*params.nx].speeds[4];
         cells[ii + jj*params.nx].speeds[3] = tmp_cells[ii + jj*params.nx].speeds[1];
@@ -309,7 +309,7 @@ int reboundCollisionAVVels(const t_param params, t_speed* cells, t_speed* tmp_ce
         cells[ii + jj*params.nx].speeds[8] = tmp_cells[ii + jj*params.nx].speeds[6];
       }
       else
-      {
+      {*/
         /* compute local density total */
         float local_density = 0.f;                 
         
@@ -392,6 +392,25 @@ int reboundCollisionAVVels(const t_param params, t_speed* cells, t_speed* tmp_ce
         tot_u += sqrtf((u_x * u_x) + (u_y * u_y));
         /* increase counter of inspected cells */
         ++tot_cells;
+      }
+    }
+  }
+
+  #pragma omp parallel for collapse(2)
+  for (int jj = 0; jj < params.ny; jj++)
+  {
+    for (int ii = 0; ii < params.nx; ii++)
+    {
+      if (obstacles[jj*params.nx + ii])
+      {
+        cells->speeds[1][ii + jj*params.nx] = tmp_cells->speeds[3][ii + jj*params.nx];
+        cells->speeds[2][ii + jj*params.nx] = tmp_cells->speeds[4][ii + jj*params.nx];
+        cells->speeds[3][ii + jj*params.nx] = tmp_cells->speeds[1][ii + jj*params.nx];
+        cells->speeds[4][ii + jj*params.nx] = tmp_cells->speeds[2][ii + jj*params.nx];
+        cells->speeds[5][ii + jj*params.nx] = tmp_cells->speeds[7][ii + jj*params.nx];
+        cells->speeds[6][ii + jj*params.nx] = tmp_cells->speeds[8][ii + jj*params.nx];
+        cells->speeds[7][ii + jj*params.nx] = tmp_cells->speeds[5][ii + jj*params.nx];
+        cells->speeds[8][ii + jj*params.nx] = tmp_cells->speeds[6][ii + jj*params.nx];
       }
     }
   }
