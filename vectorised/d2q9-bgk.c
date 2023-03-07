6e423d083dba60e354f0a8108791a8fae24ccac7
Alice <nj19721@bristol.ac.uk>
2023-03-07 14:16:21 +0000

    v

--- vectorised/d2q9-bgk.c
+++ vectorised/d2q9-bgk.c
@@ -307,7 +307,7 @@ int reboundCollisionAVVels(const t_param params, t_speed* restrict cells, t_spee
   ** the propagate step and so values of interest
   ** are in the scratch-space grid */
 
-  #pragma omp simd aligned(cells, tmp_cells) private(tot_cells, tot_u)
+  //#pragma omp simd aligned(cells, tmp_cells) private(tot_cells, tot_u)
   for (int jj = 0; jj < params.ny; jj++)
   {
     #pragma omp simd aligned(cells, tmp_cells)
