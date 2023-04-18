aa8fccd39e7798e8d780aff79d9389c41632907b
Alice <nj19721@bristol.ac.uk>
2023-04-18 19:33:51 +0100

    test

---
+++ mpi/mpi-test/mpi-test.c
@@ -0,0 +1,10 @@
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    int nprocs, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    printf("Hello from rank %d of %d\n", rank, nprocs);
+    MPI_Finalize();
+}
