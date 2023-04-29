11cff543870255585057455d26d6bd1712d7e419
Alice Ferreira <nj19721@it075767.wks.bris.ac.uk>
2023-04-29 15:46:26 +0100

    s

--- mpi/d2q9-bgk.c
+++ mpi/d2q9-bgk.c
@@ -191,11 +191,11 @@ int main(int argc, char* argv[])
   for (int tt = 0; tt < params.maxIters; tt++)
   {
     av_vels[tt] = timestep(params, cells, tmp_cells, obstacles, processData);
-#ifdef DEBUG
+//#ifdef DEBUG
     printf("==timestep: %d==\n", tt);
     printf("av velocity: %.12E\n", av_vels[tt]);
     printf("tot density: %.12E\n", total_density(params, cells));
-#endif
+//#endif
   }
   
   /* Compute time stops here, collate time starts*/
